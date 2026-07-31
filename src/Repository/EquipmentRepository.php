efbecfcb23dad59156101ccc6e8ce1c6ac9d8e14
Nicolas STRADY <nicostrady@gmail.com>
2026-07-31 21:40:27 +0200

    feat: repositories

---
+++ src/Repository/EquipmentRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Equipment;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/** @extends ServiceEntityRepository<Equipment> */
+final class EquipmentRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Equipment::class);
+    }
+}
